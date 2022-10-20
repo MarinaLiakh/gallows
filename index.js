9f9ce382bdaf9a4e9556e2951cb3dee4376228a4
Web <web@it.step>
2022-10-20 18:51:37 +0300

    change

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import randomWord from './getrandomword.js';
 
-
 const humanState = document.getElementsByClassName('humanState')[0];
 const currentWord = document.getElementsByClassName('word')[0];
 const input = document.getElementsByClassName('input')[0];
 const button = document.getElementsByClassName('button')[0];
 const spanWrapper = document.getElementsByClassName('spanWrapper')[0];
 
-const rndWord = randomWord()
+const rndWord = randomWord();
 
-const
-document.createElement('span');
+const spanArray = new Array();
+
+function createElement() {
+    let span = document.createElement('span');
+    span.innerHTML = '_';
+    spanWrapper.append(span);
+    return span;
+}
+
+for (let i = 0; i < rndWord.length; i++) {
+    spanArray.push(createElement());
+}
