05775b244319794b99163c1b1b148a0d4b3c8148
Web <web@it.step>
2022-10-20 19:47:21 +0300

    serchLetter

---
+++ serchLetter.js
@@ -0,0 +1,9 @@
+export searchLetter (word, letterToSearch) {
+    const indexArray = new Array();
+    for(let i=0; i < word.lengrth; i++ ) {
+        if(word[i] === letterToSearch) {
+            indexArray.push(i);
+        }
+    }
+    return indexArray;
+}
