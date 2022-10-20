3c109845fcac9a8bfaccabdf870bd47fd6e0fcb8
Web <web@it.step>
2022-10-20 18:36:07 +0300

    creatspan

---
+++ index.js
@@ -0,0 +1,13 @@
+import randomWord from './getrandomword.js';
+
+
+const humanState = document.getElementsByClassName('humanState')[0];
+const currentWord = document.getElementsByClassName('word')[0];
+const input = document.getElementsByClassName('input')[0];
+const button = document.getElementsByClassName('button')[0];
+const spanWrapper = document.getElementsByClassName('spanWrapper')[0];
+
+const rndWord = randomWord()
+
+const
+document.createElement('span');
